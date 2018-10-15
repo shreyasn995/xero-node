fcbff1915e165a1bb26bf3a667a0929c98d95611
Shreyas Nagarajappa <shreyas.nagarajappa@xero.com>
2018-10-15 14:50:33 +1100

    Include Payroll in index.tc

--- src/PayrollAPIClinet.ts
+++ src/PayrollAPIClinet.ts
@@ -2,11 +2,11 @@ import { BaseAPIClient, XeroClientConfiguration } from './internals/BaseAPIClien
 import { AccessToken, IOAuth1HttpClient } from './internals/OAuth1HttpClient';
 
 export class PayrollAPIClient extends BaseAPIClient {
-    public constructor(options: XeroClientConfiguration, authState?: AccessToken, _oAuth1HttpClient?: IOAuth1HttpClient) {
+	public constructor(options: XeroClientConfiguration, authState?: AccessToken, _oAuth1HttpClient?: IOAuth1HttpClient) {
 		super(options, authState, { apiBasePath: '/payroll.xro/1.0/' }, _oAuth1HttpClient);
-    }
-    
-    public employees = {
+	}
+
+	public employees = {
 		get: async (args?: { EmployeeID?: string }): Promise<object> => {
 			let endpoint = 'Employees';
 			if (args && args.EmployeeID) {
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 
 export { AccountingAPIClient } from './AccountingAPIClient';
+export { PayrollAPIClient } from './PayrollAPIClinet';
 export * from './XeroError';
